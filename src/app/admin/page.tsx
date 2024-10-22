b2fa1b9fb8b5d5a27c1ac4c8a8c30f1c5fa5abd7
devjaes <merajair17@gmail.com>
2024-10-22 10:45:58 -0500

    udpate layout

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,23 +1,24 @@
 "use client";
 import React, { use, useEffect } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 import { UserRoles } from "@/store/types/IUserResponses";
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
+import { Card } from "primereact/card";
 
 function HomePageAdmin() {
   const router = useRouter();
   const header = (
-    <img alt="Card" src="https://images.unsplash.com/photo-1664575262619-b28fef7a40a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfGFldTZyTC1qNmV3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60 " className="h-50"/>
+    <img
+      alt="Card"
+      src="https://images.unsplash.com/photo-1664575262619-b28fef7a40a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfGFldTZyTC1qNmV3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60 "
+      className="h-50"
+    />
   );
   const footer = (
     <p className="m-0 flex justify-center">
       Bienvenido! Aquí podrás administrar tu empresa.
     </p>
   );
-
 
   useEffect(() => {
     const user = window.localStorage.getItem("user");
@@ -35,15 +36,17 @@ function HomePageAdmin() {
   }, []);
 
   return (
-
-    <main className='bg-primary-Jair w-11/12'>
+    <main className="bg-primary-Jair w-11/12">
       <div className="card justify-center items-center bg-center flex h-screen">
-        <Card  header = {header} footer={footer} className="w-3/6 shadow-md border-b-2 ">
-        <h1 className="text-2xl font-bold text-center">{"POST SYSTEM"}</h1>
+        <Card
+          header={header}
+          footer={footer}
+          className="w-3/6 shadow-md border-b-2 "
+        >
+          <h1 className="text-2xl font-bold text-center">{"POS SYSTEM"}</h1>
         </Card>
       </div>
     </main>
-
   );
 }
 
